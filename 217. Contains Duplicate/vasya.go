c1826dd07994eba0729a3c8f26edaa1cdc8a4e5e
Vasily Mikhaylov <vasyanch852@gmail.com>
2026-03-09 19:37:06 +0400

    217 vasya (#4)
    * 217 vasya
    
    * 217 add task link
    
    ---------
    
    Co-authored-by: Vasiliy Mikhaylov <vvmikhaylov@avito.ru>

---
+++ 217. Contains Duplicate/vasya.go
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/contains-duplicate/description/
+package main
+
+import "fmt"
+
+// speed - O(n), memory - O(n)
+func containsDuplicate(nums []int) bool {
+	freq := make(map[int]int, len(nums))
+	for _, elem := range nums {
+		freq[elem] = freq[elem] + 1
+		if freq[elem] > 1 {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(containsDuplicate([]int{1, 2, 3, 1}))
+	fmt.Println(containsDuplicate([]int{1, 2, 3}))
+}
